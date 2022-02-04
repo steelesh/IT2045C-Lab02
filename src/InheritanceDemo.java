28bdcf5c57835d342e586f6006b56a0800dc2657
Steele Shreve <sshreve0714@icloud.com>
2022-02-04 18:06:53 -0500

    Easier readability + JavaDoc comments in Person class.

--- src/InheritanceDemo.java
+++ src/InheritanceDemo.java
@@ -4,24 +4,26 @@ public class InheritanceDemo {
 
     public static void main(String[] args) {
 
-        Worker worker1 = new Worker("000001", "John", "Doe", "Mr.", 2000, 10.75);
-        Worker worker2 = new Worker("000002", "Jane", "Doe", "Mrs.", 1990, 18.50);
-        Worker worker3 = new Worker("000003", "Tom", "Smith", "Mr.", 1980, 22.75);
-        SalaryWorker salWorker1 = new SalaryWorker("000004", "Bob", "Jones", "Mr.", 1950, 15.50, 31500);
-        SalaryWorker salWorker2 = new SalaryWorker("000005", "Thomas", "Williams", "Mr.", 2000, 18.75, 37600);
-        SalaryWorker salWorker3 = new SalaryWorker("000006", "Steve", "Brown", "Mr.", 1960, 32.50, 72500);
+        Worker worker1 = new Worker("000001", "John", "Doe", "Mr.", 2000, 10.75); // Worker obj.
+        Worker worker2 = new Worker("000002", "Jane", "Doe", "Mrs.", 1990, 18.50); // Worker obj.
+        Worker worker3 = new Worker("000003", "Tom", "Smith", "Mr.", 1980, 22.75); // Worker obj.
 
-        ArrayList<Worker> workers = new ArrayList<>();
+        SalaryWorker salWorker1 = new SalaryWorker("000004", "Bob", "Jones", "Mr.", 1950, 15.50, 31500); // SalaryWorker obj.
+        SalaryWorker salWorker2 = new SalaryWorker("000005", "Thomas", "Williams", "Mr.", 2000, 18.75, 37600); // SalaryWorker obj.
+        SalaryWorker salWorker3 = new SalaryWorker("000006", "Steve", "Brown", "Mr.", 1960, 32.50, 72500); // SalaryWorker obj.
 
-        workers.add(worker1);
-        workers.add(worker2);
-        workers.add(worker3);
-        workers.add(salWorker1);
-        workers.add(salWorker2);
-        workers.add(salWorker3);
+        ArrayList<Worker> workers = new ArrayList<>(); // Create new ArrayList of type Worker to store created worker objects
 
-        System.out.println("-------------------------------------------------------------------------------------");
-        for(int i = 0; i < workers.size(); i++){
+        workers.add(worker1); // Add worker
+        workers.add(worker2); // Add worker
+        workers.add(worker3); // Add worker
+        workers.add(salWorker1); // Add SalaryWorker
+        workers.add(salWorker2); // Add SalaryWorker
+        workers.add(salWorker3); // Add SalaryWorker
+
+        System.out.println("-------------------------------------------------------------------------------------"); // Top of tabular display
+        for(int i = 0; i < workers.size(); i++){ // For loop
+            // Print worker info + weekly pay
             Worker worker = (Worker) workers.get(i);
             System.out.printf("%-30s", worker.formalName());
             System.out.printf("%-20s", "Week One");
@@ -36,7 +38,7 @@ public class InheritanceDemo {
             System.out.printf("%-13s", " ");
             System.out.print("$" + worker.displayWeeklyPay(40));
             System.out.println();
-            System.out.println("-------------------------------------------------------------------------------------");
+            System.out.println("-------------------------------------------------------------------------------------"); // Bottom of tabular display
         }
     }
 }
